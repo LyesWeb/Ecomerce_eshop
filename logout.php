99bd125bf10ce4fd3dac95ee1497dd2a1fc4f53d
Lyes <ilyas.ariba@gmail.com>
2017-05-07 02:14:37 +0000

    first vesion 1.0

--- logout.php
+++ logout.php
@@ -1,11 +1,4 @@
 <?php
-# ecomerce-eshop
-# this is a simple and basic script for ecomerce websites coded by : Ilyas ARIBA
-# Email : ilyas.ariba@gmail.com
-# Facebook : facebook.com/ilyas.ariba
-# Twitter : twitter.com/ilyas_ariba
-# Behance : behance.net/ilyas-ariba
-# Instagram : instagram.com/ilyas.ariba
 ?>
 <?php
 session_start();
